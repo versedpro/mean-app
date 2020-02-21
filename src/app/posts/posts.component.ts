8181e4f73e2ff8ea7e3da6a13427260f194c0c2d
Wishtopdev <ultraj0330@gmail.com>
2020-02-21 06:08:29 -0800

    second

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,14 +9,21 @@ import { PostsService } from '../posts.service';
 export class PostsComponent implements OnInit {
   // instantiate posts to an empty array
   posts: any = [];
-
+  name: string = "hello";
   constructor(private postsService: PostsService) { }
 
-  ngOnInit() {
-    // Retrieve posts from the API
+  goToApi() {
+    
     this.postsService.getAllPosts().subscribe(posts => {
       this.posts = posts;
+      console.log(this.posts)
     });
+    
   }
-
+  ngOnInit() {
+    // Retrieve posts from the API
+    // this.postsService.getAllPosts().subscribe(posts => {
+    //   this.posts = posts;
+    // });
+  }
 }
